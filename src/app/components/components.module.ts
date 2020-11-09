a7ded7e03ef0b1c518ea2a8f232e9e472b7411d7
jonatas.lab3 <jonatas.evangelista@lab3dvlp.com>
2020-11-09 12:28:01 -0300

    Adicionado componente de animação de loading (progress-bar).

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,19 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AvatarComponent } from './avatar/avatar.component';
 import { ProfessionalInformationComponent } from './professional-information/professional-information.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 
 @NgModule({
-  declarations: [AvatarComponent, ProfessionalInformationComponent],
-  exports: [AvatarComponent, ProfessionalInformationComponent],
+  declarations: [
+    AvatarComponent,
+    ProfessionalInformationComponent,
+    ProgressBarComponent,
+  ],
+  exports: [
+    AvatarComponent,
+    ProfessionalInformationComponent,
+    ProgressBarComponent,
+  ],
   imports: [CommonModule],
 })
 export class ComponentsModule {}
